0d1494b52cb32b8922fbb54615dd99a4305ddced
Oleksii Kulikov <yeexel@gmail.com>
2019-06-22 00:34:55 +0200

    Test serverless s.js

--- s.js
+++ s.js
@@ -2,6 +2,8 @@ const express = require("express");
 const path = require("path");
 const app = express();
 
+app.use("/static", express.static(path.join(__dirname, "build", "static")));
+
 app.get("*", function(req, res) {
   res.sendFile(path.join(__dirname, "build", "index.html"), {
     headers: {
