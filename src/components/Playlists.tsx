ec451e5e31a25f9a23478421fcbf424a74f15eca
Oleksii Kulikov <yeexel@gmail.com>
2019-09-18 12:39:08 +0200

    check for playlist image

--- src/components/Playlists.tsx
+++ src/components/Playlists.tsx
@@ -3,7 +3,7 @@ import styled from "styled-components";
 import { getPlaylists } from "../infrastructure/api";
 import { withAuthContext } from "../infrastructure/AuthContext";
 import Record from "../img/record.png";
-import posed from 'react-pose';
+import posed from "react-pose";
 
 class Playlists extends React.Component<any, any> {
   state = {
@@ -13,14 +13,14 @@ class Playlists extends React.Component<any, any> {
     initialLoad: true,
     limit: 20,
     offset: 0
-  }
+  };
 
   el: any;
 
   async componentDidMount() {
     await this._fetchPlaylists();
 
-    this.setState({ initialLoad: false })
+    this.setState({ initialLoad: false });
 
     window.addEventListener("scroll", this.scrollEventHandler);
   }
@@ -36,10 +36,13 @@ class Playlists extends React.Component<any, any> {
     scrollHeight = document.body.scrollHeight;
     totalHeight = window.scrollY + window.innerHeight;
 
-    if(totalHeight >= scrollHeight && hasMore) {
-      this.setState({ offset: this.state.offset + this.state.limit }, this._fetchPlaylists);
+    if (totalHeight >= scrollHeight && hasMore) {
+      this.setState(
+        { offset: this.state.offset + this.state.limit },
+        this._fetchPlaylists
+      );
     }
-  }
+  };
 
   render() {
     const { playlists, isLoading, initialLoad } = this.state;
@@ -49,17 +52,35 @@ class Playlists extends React.Component<any, any> {
     // }
 
     return (
-      <Container pose={initialLoad ? 'closed' : 'open'}>
-          {playlists.map((playlist: any, index: number) => {
-            return (
-              <PlaylistContainer onClick={() => this.props.history.push(`/playlist/${playlist.id}`)} key={playlist.id} firstChild={index === 0}>
-                <PlaylistImage src={playlist.images[0].url} />
-                <RecordImage style={{ position: 'absolute' }} id="rec" width="250" height="250" src={Record} />
-                <PlaylistTitle>{playlist.name}</PlaylistTitle>
-              </PlaylistContainer>
-            )
-          })}
-        </Container>
+      <Container pose={initialLoad ? "closed" : "open"}>
+        {playlists.map((playlist: any, index: number) => {
+          return (
+            <PlaylistContainer
+              onClick={() =>
+                this.props.history.push(`/playlist/${playlist.id}`)
+              }
+              key={playlist.id}
+              firstChild={index === 0}
+            >
+              <PlaylistImage
+                src={
+                  playlist.images && playlist.images[0].hasOwnProperty("url")
+                    ? playlist.images[0].url
+                    : ""
+                }
+              />
+              <RecordImage
+                style={{ position: "absolute" }}
+                id="rec"
+                width="250"
+                height="250"
+                src={Record}
+              />
+              <PlaylistTitle>{playlist.name}</PlaylistTitle>
+            </PlaylistContainer>
+          );
+        })}
+      </Container>
     );
   }
 
@@ -69,19 +90,20 @@ class Playlists extends React.Component<any, any> {
 
     this.setState({ isLoading: true });
 
-    const playlistData = await getPlaylists(limit, offset, authContext.authToken);
+    const playlistData = await getPlaylists(
+      limit,
+      offset,
+      authContext.authToken
+    );
 
-    const playlists = [
-      ...this.state.playlists,
-      ...playlistData.items
-    ];
+    const playlists = [...this.state.playlists, ...playlistData.items];
 
     this.setState({
       isLoading: false,
       playlists,
       hasMore: playlists.length < playlistData.total
     });
-  }
+  };
 }
 
 export default withAuthContext(Playlists);
@@ -138,15 +160,18 @@ const PlaylistContainer = styled.div<{ firstChild: boolean }>`
 
   @media (max-width: 500px) {
     height:300px;
-    // margin-left: ${props => props.firstChild ? Math.ceil((document.documentElement.clientWidth - 250) / 2) - 10 : 0}px;
+    // margin-left: ${props =>
+      props.firstChild
+        ? Math.ceil((document.documentElement.clientWidth - 250) / 2) - 10
+        : 0}px;
   }
-`
+`;
 
 const PlaylistImage = styled.img`
   width: 250px;
   height: 250px;
   z-index: 1;
-  transition: .5s ease;
+  transition: 0.5s ease;
   // border: 2px solid #fff;
   border-radius: 5px;
   box-shadow: 0 8px 6px -6px black;
@@ -161,10 +186,10 @@ const PlaylistImage = styled.img`
       margin: 0;
     }
   }
-`
+`;
 
 const RecordImage = styled.img`
-  transition: .75s ease;
+  transition: 0.75s ease;
   width: 240px;
   height: 240px;
   padding: 5px;
@@ -177,7 +202,7 @@ const RecordImage = styled.img`
   @media (max-width: 500px) {
     display: none;
   }
-`
+`;
 
 const PlaylistTitle = styled.span`
   align-self: center;
@@ -197,9 +222,9 @@ const PlaylistTitle = styled.span`
   @media (max-width: 500px) {
     opacity: 1;
   }
-`
+`;
 
 const PlaylistCreatedBy = styled.span`
   align-self: center;
   color: #fff;
-`
+`;
